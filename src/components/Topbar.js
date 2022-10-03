ecac4328ab9e73eed3be55e4ae18f8b87b60b42a
Ken Leung <kenster99@gmail.com>
2022-10-03 16:20:00 -0400

    added minimal Topbar, Home

---
+++ src/components/Topbar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import CssBaseline from '@mui/material/CssBaseline';
+import AppBar from '@mui/material/AppBar';
+
+const Topbar = () => {
+  return (
+    <div className='Topbar'>Topbar</div>
+  )
+}
+
+export default Topbar
