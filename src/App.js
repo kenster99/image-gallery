c7d16e1fb6b3b1f9a12e8faed32bd6e4c9a3546f
Ken Leung <kenster99@gmail.com>
2022-10-04 21:41:55 -0400

    basic user context working in Topbar component

--- src/App.js
+++ src/App.js
@@ -8,11 +8,13 @@ import Amplify from 'aws-amplify';
 import awsExports from './aws-exports';
 import { Link } from 'react-router-dom';
 import { Routes, Route} from 'react-router-dom';
+import { useAuthenticator } from '@aws-amplify/ui-react';
 
 Amplify.configure(awsExports)
 
-function App({ signOut, user }) {
+function App() {
 
+  const { user, signOut } = useAuthenticator((context) => [context.user]);
   return <Routes>
     <Route path="/" element={<Home />} />
     <Route path="/uploadImage" element={<UploadImage />} />
--- src/components/Topbar.js
+++ src/components/Topbar.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import CssBaseline from '@mui/material/CssBaseline';
 import AppBar from '@mui/material/AppBar';
+import { useAuthenticator } from '@aws-amplify/ui-react';
 
 const Topbar = () => {
+  const { user, signOut } = useAuthenticator((context) => [context.user]);
+
   return (
-    <div className='Topbar'>Topbar</div>
+    <div className='Topbar'>Topbar {user.attributes.email}</div>
   )
 }
 
