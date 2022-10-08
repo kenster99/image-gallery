e5184e4207db7db4d39d1bbe403061c591805c43
Ken Leung <kenster99@gmail.com>
2022-10-08 14:26:16 -0400

    working image gallery

--- src/pages/Catalog.js
+++ src/pages/Catalog.js
@@ -5,6 +5,9 @@ import { API, graphqlOperation, Storage } from 'aws-amplify'
 import { listImages } from '../graphql/queries'
 import { Box } from '@mui/system';
 import { DataGrid } from '@mui/x-data-grid';
+import { AmplifyS3Image } from '@aws-amplify/ui-react/legacy';
+import './Catalog.css';
+import Actions from '../components/Actions';
 
 const Catalog = () => {
     const [images, setImages] = useState([])
@@ -23,45 +26,48 @@ const Catalog = () => {
     console.log("== images == ")
     console.log(images)   
     const columns = [
-        // { field: "url", headerName: "", height: "500px", sortable: false, filterable: false, 
-        //     renderCell: (params) => {
-        //         return (
-        //             <>
-        //                 <img src={params.value} alt={item.file.key} height="100%" width="100%"  />
-        //             </>
-        //         )
-        //     } 
-        // },
-        { field: "url", headerName: "URL", flex: 1},        
+        { field: "url", headerName: "", sortable: false, filterable: false, 
+            renderCell: (params) => {
+                return (
+                    <>
+                        <AmplifyS3Image imgKey={params.value} height="100" width="150" />
+                    </>
+                )
+            } 
+        },
+        // { field: "url", headerName: "URL", flex: 1},        
+        { field: "filename", headerName: "Filename", flex: 1},
         { field: "id", headerName: "ID", flex: 1},
         { field: "created", headerName: "Created", flex: 1},
-        // { field: "action", headerName: "Action", type: "actions", 
-        //     renderCell: (params) => <Actions {...{params, rowId, setRowId}} /> }
+        { field: "action", headerName: "Action", type: "actions", 
+            renderCell: (params) => <Actions {...{params, rowId, setRowId}} /> }
     ]
 
-    // function getFileUrl (file) {
-    //     const fileBlob = Storage.get(file.key)
-    //     // return URL.createObjectURL(fileBlob)
-    //     return fileBlob
-    // }
+    async function getFileUrl (file) {
+        const fileBlob = await Storage.get(file.key)
+        // return URL.createObjectURL(fileBlob)
+        return fileBlob
+    }
+    
     const rows = images.map(image => ({
         id: image.id,
-        url: image.file.key,
+        filename: image.name,
+        url: image.name,
         created: image.createdAt
     }))
+
     console.log("== rows == ")
     console.log(rows)  
   return (
     <>
     <Topbar />
-    <div>Catalog</div>
-    <Box sx={{display:'flex', mx:'auto', margin:8, height:500, width:'90%'}}>
-        {/* <DataGrid rows={rows} columns={columns} pageSize={10} pagination={true} 
+    <Box sx={{display:'flex', mx:'auto', my:'auto', margin:8, height:500, width:'90%'}}>
+        <DataGrid rows={rows} columns={columns} pageSize={10} pagination={true} rowHeight={120}
             getRowId={(row) => row.id}
             initialState= {{
                 sorting: { sortModel: [{field: 'created', sort: 'desc'}]}
             }}
-            onCellEditCommit={(params) => setRowId(params.id)} /> */}
+            onCellEditCommit={(params) => setRowId(params.id)} />
     </Box>
     </>
   )
