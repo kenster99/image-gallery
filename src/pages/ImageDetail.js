21e1618fff0b8ef4f8b44998a7053ffa0528186d
Ken Leung <kenster99@gmail.com>
2022-10-10 21:51:35 -0400

    sometimes image data populates...wip

--- src/pages/ImageDetail.js
+++ src/pages/ImageDetail.js
@@ -1,10 +1,14 @@
-import React, { useEffect, useState } from 'react'
+import React, { useLayoutEffect, useState } from 'react'
 import { API, graphqlOperation, Storage } from 'aws-amplify'
 import { useParams } from 'react-router-dom'
 import { getImage } from '../graphql/queries'
+import { Grid, Box, TextField } from '@mui/material'
+import { AmplifyS3Image } from '@aws-amplify/ui-react/legacy';
+import Topbar from '../components/Topbar'
+
 
 const ImageDetail = () => {
-    const [image, setImage] = useState() 
+    const [image, setImage] = useState(null) 
     const { id } = useParams()
 
     async function fetchData() {
@@ -15,18 +19,42 @@ const ImageDetail = () => {
           variables: { id }
         })
 
-        const imageData = apiData.data.getImage;
-        setImage(imageData);
-        console.log(imageData)
+        setImage(apiData.data.getImage);
+
     };
 
-    useEffect( () => {
+    useLayoutEffect( () => {
         fetchData();      
-    },[image]);
-    
+    },[]);
+
+    console.log('image: ', image)
   return (
-    <div className='ImageDetail'>
-        
+    <div>
+      <Topbar />
+      <Grid container spacing={2} margin={4}>
+        <Grid xs={4}>
+          <AmplifyS3Image imgKey={image.name} height="100%" width="100%" />
+        </Grid>
+        <Grid xs={8}>
+          <Box
+            component="form"
+            sx={{
+              '& .MuiTextField-root': { m: 1, width: '25ch' },
+            }}
+            noValidate
+            autoComplete="off"
+          >
+            <div>
+              <TextField
+                required
+                id="outlined-required"
+                label="Required"
+                defaultValue="Hello World"
+              />              
+            </div>
+          </Box>
+        </Grid>          
+      </Grid>
     </div>
   )
 }
